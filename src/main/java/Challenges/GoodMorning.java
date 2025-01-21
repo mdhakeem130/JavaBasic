693d480338045e20459cbb15667bd8206904f8f0
Your Name <you@example.com>
2025-01-21 20:50:01 +0530

    First Project program

--- src/main/java/Challenges/GoodMorning.java
+++
@@ -1,7 +0,0 @@
-package Challenges;
-
-public class GoodMorning {
-    public static void main(String[] args) {
-        System.out.println("Good Morning");
-    }
-}
--- src/main/java/com/java/basic1/FirstProgram.java
+++
@@ -1,7 +0,0 @@
-package com.java.basic1;
-
-public class FirstProgram {
-    public static void main(String[] args) {
-        System.out.println("My First Program" );
-    }
-}
