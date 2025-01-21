5452708a5f66f9e87375489e28a0f80c79f464cc
Your Name <you@example.com>
2025-01-21 21:18:36 +0530

    My 2 Challange with question

--- Challenges/Subscribe/src/Main.java
+++ Challenges/Subscribe/src/Main.java
@@ -1,5 +1,5 @@
 public class Main {
-
+//2 Craete a new Project in Intelij  Idea and output Subscribe on the console
     public static void main(String[] args) {
         System.out.println("Subscribe!");
     }
