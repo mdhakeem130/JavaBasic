4b8e5e27b5e32f2761b93aa344706a5b44dfce12
Your Name <you@example.com>
2025-01-23 21:56:34 +0530

    Variable 3

---
+++ Challenges/Patterns/src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    //3 show the patterns program 1 right half pyramid
+    public static void main(String[] args) {
+        System.out.println("*");
+        System.out.println("* *");
+        System.out.println("* * *");
+        System.out.println("* * * *");
+        System.out.println("* * * * *");
+    }
+}
