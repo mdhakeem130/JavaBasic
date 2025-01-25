b7bc1dd5fd811b55d6848d01fef1296285056e57
Your Name <you@example.com>
2025-01-25 12:42:30 +0530

    commit all challanges before id else

---
+++ 3Data Type And Variable/Challange/src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+    public static void main(String[] args) {
+       // show the patterns program 1 right half pyramid
+        // ReverseRightHalfPyramid and LeftHalfPyramid  using single print
+        //1
+       // System.out.println("*\n* *\n* * *\n* * * *\n* * * * * ");
+       // System.out.println("\n");
+       // System.out.println("* * * * * \n* * * *\n* * *\n* * \n*");
+       // System.out.println("\n");
+        System.out.println("        * \n      * *\n    * * *\n  * * * * \n* * * * *");
+
+
+
+    }
+}
